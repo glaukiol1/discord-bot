d20310534ef370400f45fa52401394e8fa247499
glaukiol1 <glaukiol1@gmail.com>
2021-09-30 20:48:38 +0200

    Fix typo

--- json.js
+++ json.js
@@ -4,7 +4,7 @@ module.exports = class json {
     constructor(filename) {
         this.filename = filename;
         this.file = fs.readFileSync(path.join(__dirname, this.filename), 'utf8')
-        this.data = JSON.parse(file);
+        this.data = JSON.parse(this.file);
         this.update = () => {
             fs.writeFileSync(
                 path.join(__dirname, this.filename),
